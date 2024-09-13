68ae3c1dd6c9e15218e826eac5b2ebe2ce7690b1
aaron.feingold <ajfeingold88@gmail.com>
2024-09-13 17:08:55 -0500

    clean data and better fetching

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from datetime import datetime
 
-def scraper(event, context):
-  url = "https://www.wwoz.org/calendar/livewire-music"
 
-  html_page = urlopen(Request(url))
+def scrape_wwoz(event, context):
+    url = f"https://www.wwoz.org/calendar/livewire-music?date={datetime.now().date().strftime('%Y-%m-%d')}"
+    html_page = urlopen(Request(url))
+    soup = BeautifulSoup(html_page, "html.parser")
 
-  soup = BeautifulSoup(html_page, features="html.parser")
+    links = []
+    # Find the 'livewire-listing' class specifically
+    livewire_listing = soup.find("div", class_="livewire-listing")
+    # Find only the 'panel panel-default' divs within the 'livewire-listing' div
+    if livewire_listing:
+        panels = livewire_listing.find_all("div", class_="panel panel-default")
+    else:
+        print("Error: No livewire-listing found on the page.")
+        return []
 
-  links = []
+    for panel in panels:
+        for row in panel.find_all("div", class_="row"):
+            artist_link = row.find("div", class_="calendar-info").find("a")
 
-  for link in soup.find_all('a'):
-    alike = "/events/"
-    event_href = str(link.get('href'))
-    artist_name  = str(link.renderContents()).replace("b'\\n", "").replace("'", "").strip()
-    artist_event = {}
+            links.append({artist_link.text.strip(): artist_link["href"]})
 
-    if alike in event_href:
-      artist_event[artist_name] = event_href
-      links.append(artist_event)
-
-  return links
-
+    return links
