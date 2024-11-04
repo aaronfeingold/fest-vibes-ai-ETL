4796882dcd331dda63d68f28b821a488c46b65a8
aaron.feingold <ajfeingold88@gmail.com>
2024-11-04 11:09:49 -0600

    invoke test

---
+++ tests/test_invoke.py
@@ -0,0 +1,23 @@
+import json
+from main import lambda_handler
+
+
+# Simulated AWS Lambda context
+class LambdaTestContext:
+    aws_request_id = "test-request-id"
+    log_stream_name = "test-log-stream"
+    function_name = "test-function"
+    function_version = "1.0"
+    memory_limit_in_mb = 128
+    invoked_function_arn = (
+        "arn:aws:lambda:us-west-2:123456789012:function:test-function"
+    )
+    remaining_time_in_millis = 30000  # Simulated remaining time
+
+
+# Simulated event data
+event = {"queryStringParameters": {"date": "2024-11-01"}}
+
+# Invoke the handler and print the result
+result = lambda_handler(event, LambdaTestContext())
+print(json.dumps(result, indent=4))
